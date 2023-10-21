4531720e72b8ec62d91fbfbff2c5ccb0e9a3c3c1
Yusha Tahlil <ymmttmy5@gmail.com>
2023-10-21 11:54:21 +0530

    Expense controller complete

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -12,6 +12,7 @@ const addExpense = asyncHandler(async (req, res, next) => {
 
   const session = await mongoose.startSession();
   session.startTransaction();
+
   try {
     await Expense.create(
       [
@@ -35,7 +36,9 @@ const addExpense = asyncHandler(async (req, res, next) => {
         totalExpenses,
       }
     ).session(session);
+
     await session.commitTransaction();
+
     res.status(201).json({
       success: true,
       messsage: "Expense created Successfully!",
@@ -53,66 +56,66 @@ const getUserExpenses = asyncHandler(async (req, res, next) => {
   const page = parseInt(req.query.page) || 1;
   const perPage = parseInt(req.query.rows) || 10;
 
-  const offset = (page - 1) * perPage;
-  const result = await Expense.findAndCountAll({
-    where: {
-      userId: req.user._id,
-    },
-    attributes: {
-      exclude: ["userId"],
-    },
-    limit: perPage,
-    offset: offset,
-    order: [["createdAt", "DESC"]],
-  });
-  const { count, rows } = result;
+  const skip = (page - 1) * perPage;
 
-  const totalPages = Math.ceil(count / perPage);
-  res.status(200).json({
-    expenses: rows,
-    currentPage: page,
-    totalPages: totalPages,
-  });
+  try {
+    const count = await Expense.countDocuments({ userId: req.user._id });
+    const expenses = await Expense.find({ userId: req.user._id })
+      .select({ userId: 0 })
+      .limit(perPage)
+      .skip(skip)
+      .sort({ createdAt: -1 });
+
+    const totalPages = Math.ceil(count / perPage);
+
+    res.json({
+      expenses,
+      currentPage: page,
+      totalPages,
+    });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Something went Wrong!");
+  }
 });
 
 const deleteUserExpense = asyncHandler(async (req, res, next) => {
-  const _id = req.params.id;
-  if (!_id) {
+  const id = req.params.id;
+  if (!id) {
     res.status(400);
     throw new Error("Please provide an Id!");
   }
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
-    const t = await sequelize.transaction(); //starting a transaction
-    //find the expense to be deleted
     const expenseToDelete = await Expense.findOne({
-      where: {
-        userId: req.user._id,
-        _id: _id,
-      },
-      transaction: t,
-    });
+      userId: req.user._id,
+      _id: id,
+    }).session(session);
 
     if (!expenseToDelete) {
       res.status(404);
       throw new Error("Expense not found");
     }
-    //update user
-    await User.decrement("totalExpenses", {
-      by: expenseToDelete.amount,
-      where: {
-        _id: req.user._id,
-      },
-      transaction: t,
-    });
-    await expenseToDelete.destroy({ transaction: t });
-    await t.commit(); //save changes
-    res
-      .status(200)
-      .json({ success: true, message: "Expense deleted successfully" });
+    // Update the user's totalExpenses
+    await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $inc: { totalExpenses: -expenseToDelete.amount } },
+      { session }
+    );
+
+    await expenseToDelete.deleteOne({ session }); // Delete the expense
+    await session.commitTransaction();
+
+    res.json({ success: true, message: "Expense deleted successfully" });
   } catch (error) {
-    await t.rollback();
+    await session.abortTransaction();
     res.status(500);
     throw new Error("Something went Wrong!");
+  } finally {
+    session.endSession();
   }
 });
 
